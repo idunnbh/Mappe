64ec460a740ce4dc777cf8515735e59dbdc9b437
Idunn Hollaas <idunnbh@stud.ntnu.no>
2025-03-27 13:58:52 +0100

    Valgt ut hvilke verdier jeg vil ha med til ny df

--- src/Databehandling_Idunn.py
+++ src/Databehandling_Idunn.py
@@ -11,6 +11,24 @@ with open("data/luftkvalitet.json", "r", encoding="utf-8") as f:
 df = process_air_quality_data(raw_data)
 
 #Får ut info om dataen 
-print("\n--- DataFrame info ---")
-df.info()
+#print("\n--- DataFrame info ---")
+#df.info()
     
+selected_columns = [
+    "from",
+    "to",
+    "variables.pm10_concentration.value",
+    "variables.pm25_concentration.value",
+    "variables.no2_concentration.value",
+    "variables.o3_concentration.value",
+    "variables.AQI.value",
+]
+df_selected = df[selected_columns]
+
+#Får ut info om den nye dataframen 
+print("\n--- Ny DataFrame info ---")
+df_selected.info()
+
+
+
+print(df_selected)

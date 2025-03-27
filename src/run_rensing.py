4145f842594eabb54d550bf0c61b9b1dda770c38
victogr <victoriag00@outlook.com>
2025-03-27 15:27:26 +0100

    Har fikset rensingskode og oppdatert hvor rensingen skjer. Slettet hvor gamle rensingen lå før. Har også skrevet inn på README om behanlingen av Klimagassutslipp-dataen og hvordan det har foregått.

--- src/run_rensing.py
+++ src/run_rensing.py
@@ -1,18 +1,23 @@
 import pandas as pd
+import os
 from databehandling.rensing_temperaturdata import rens_data, klimagass_rens
 
 df_temp = pd.read_csv("data/gloshaugen_temperaturer.csv")
 
 #Renser data
-df_temp = rens_data(df)
+df_temp = rens_data(df_temp)
 
 #Lager er renset csv fil 
 df_temp.to_csv("data/temperatur_renset.csv", index=False)
 print("Renset data lagret i data/temperatur_renset.csv")
 
-
     # Kaller CSV om klimagassutslipp og renser den
-df_klima = pd.read_csv("data/klimagassutslipp.csv", sep=";", encoding="utf-8", skiprows=2)
-df_temp = klimagass_rens
-df_temp.to_csv("data/klimagassutslipp_renset.csv", index=False)
-print("Renset data lagret i data/klimagassutslipp_renset.csv")
+klimagass_renset_path = "data/klimagassutslipp_renset.csv"
+
+if not os.path.exists(klimagass_renset_path):
+    df_klima = pd.read_csv("data/klimagassutslipp.csv", sep=";", encoding="utf-8", skiprows=2)
+    df_klima = klimagass_rens(df_klima)
+    df_klima.to_csv("data/klimagassutslipp_renset.csv", index=False)
+    print("Renset data lagret i data/klimagassutslipp_renset.csv")
+else:
+    print("Klimagassdata er allerede renset.")

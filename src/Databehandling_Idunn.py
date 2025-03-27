27cada615e36cfeae881c231a26d31aa68a5b81e
Idunn Hollaas <idunnbh@stud.ntnu.no>
2025-03-27 12:39:16 +0100

    Fjernet unødvendige funksjoner

--- src/Databehandling_Idunn.py
+++ src/Databehandling_Idunn.py
@@ -4,16 +4,13 @@ import pandas as pd
 
 from Datainnsamling_Idunn import process_air_quality_data
 
+#Leser fra luftkvalitet.json
 with open("data/luftkvalitet.json", "r", encoding="utf-8") as f:
     raw_data = json.load(f)
 
 df = process_air_quality_data(raw_data)
 
+#Får ut info om dataen 
 print("\n--- DataFrame info ---")
 df.info()
     
-print("\n--- DataFrame describe (bare numeriske kolonner) ---")
-print(df.describe())
-    
-print("\n--- DataFrame columns ---")
-print(df.columns)

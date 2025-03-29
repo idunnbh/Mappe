ac9798c9db7da636d88528cf6b9774ad4d3a6c5d
johannlw <johannlw@stud.ntnu.no>
2025-03-29 15:04:39 +0100

    La til SQL på DataFrames med pandasql

--- src/rensing.py
+++ src/rensing.py
@@ -1,13 +1,22 @@
 import pandas as pd
+from pandasql import sqldf
+
 
 def last_in_csv(filsti):
     return pd.read_csv(filsti)
 
+def finn_gjennomsnittstemperatur(df):
+    pysqldf = lambda q: sqldf(q, {'df': df})
+    query = "SELECT AVG(temperatur) as snitt_temp FROM df WHERE temperatur IS NOT NULL"
+    result = pysqldf(query)
+    return result['snitt_temp'].iloc[0]
+
 def håndter_manglende_verdier(df, kolonne='temperatur'):
     manglende_verdier= df[kolonne].isnull().sum()
     if manglende_verdier > 0:
-        print(f"Manglende verdier={manglende_verdier}.Disser er nå fylt med gjennomsnittet.")
-    df[kolonne] = df[kolonne].fillna(df[kolonne].mean())
+        gjennomsnitt = finn_gjennomsnittstemperatur(df)
+        print(f"Manglende verdier={manglende_verdier}. Disser er nå fylt med gjennomsnittet:{gjennomsnitt}")
+        df[kolonne] = df[kolonne].fillna(gjennomsnitt)
     return df
 
 def fjern_duplikater(df):

fa615112eec2813bff2cb8aad94b019405018306
Idunn Hollaas <idunnbh@stud.ntnu.no>
2025-04-23 12:05:10 +0200

    Opprettet egne filer for visualisering av temperatur og luftkvalitet og la inn funksjonen for plottet i Visualisering.ipynb

--- src/luftkvalitet_visualisering.py
+++ src/luftkvalitet_visualisering.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+
 from datainnsamling_luftkvalitet import df_valid
 
+import dataanalyse
+import importlib
+importlib.reload(dataanalyse)
+
+
+ 
 df_hist = df_valid.copy()
 
 # Konverterer til Pandas datetime-objekter og sorterer kronologisk
@@ -13,14 +20,15 @@ df_hist['Tid'] = pd.to_datetime(
 )
 df_hist = df_hist.sort_values("Tid")
 
-# Plotter linjediagram for luftkvalitetsvarsel
+# Plotter linjediagram for historisk luftkvalitet
 plt.figure(figsize=(12, 6))
 sns.lineplot(data=df_hist, x="Tid", y="Elgeseter PM10 µg/m³ Day", label="PM10")
 sns.lineplot(data=df_hist, x="Tid", y="Elgeseter PM2.5 µg/m³ Day", label="PM2.5")
 sns.lineplot(data=df_hist, x="Tid", y="Elgeseter NO2 µg/m³ Day", label="NO₂")
 plt.xlabel("Tid")
 plt.ylabel("Konsentrasjon (µg/m³)")
-plt.title("Luftkvalitetsvarsel")
+plt.title("Luftkvalitet siste 20 år")
 plt.xticks(rotation=45)
-plt.show()
+#plt.show()
+
 
